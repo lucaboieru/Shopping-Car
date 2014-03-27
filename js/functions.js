a5b6a474300b6d1cb106ffce75fba18ed00a2d9c
Luca Boieru <luca.boieru@yahoo.com>
2014-03-27 02:19:57 +0200

    Created first version of the app.

---
+++ js/functions.js
@@ -0,0 +1,146 @@
+var json;
+
+$(document).ready(function () {
+
+    // load first tab
+    $('.tab:first').show();
+    $(".pageTitle").html("Categories");
+
+    loadCategories();
+
+    // tab select handler
+    $('.navItem').on('click', function () {
+
+        // get the right tab
+        var tab = $(this).attr('data-action');
+
+        $('.navItem').removeClass('active');
+        $(this).addClass('active')
+
+        // hide all tabs
+        $('.tab').hide();
+
+        // show the right tab
+        var $currentTab = $('#' + tab);
+        $currentTab.fadeIn(150);
+        $(".pageTitle").html(capitaliseFirstLetter(tab));
+
+        if (tab === 'categories') {
+            // TODO we won't need this after the back button will work
+            loadCategories();
+        }
+    });
+
+    $(document).on('click', '.category', function() {
+        if (!$(this).attr("subcategory")) {
+            showBackButton();
+            loadSubcategories($(this).attr("category"));
+        } else {
+            loadProductList($(this).attr("category"), $(this).attr("subcategory"));
+            showBackButton();
+        }
+    });
+
+    document.addEventListener("backbutton", onBackKeyDown, false);
+
+    function onBackKeyDown () {
+        alert("You pressed the back button on your Android device. Good Job! ;)");
+    }
+});
+
+function loadProductList (category, subcategory) {
+
+    var params = {
+        "class": category,
+        "subclass": subcategory
+    };
+
+    makeAjaxPostCall("", params, function (data) {
+        var elems = [];
+        var products = JSON.parse(data);
+        for(var i in products) {
+            var $temp = $(".product-temp").clone();
+
+            // add data to product template
+            $temp.find(".product-img").attr("src", products[i].img);
+            $temp.find(".price-tag").html(products[i].price);
+            $temp.find(".old-price").html(products[i].oldPrice);
+            $temp.find(".product-description .title").html(products[i].title);
+
+            $temp.removeClass("product-temp");
+            $temp.show();
+            elems.push($temp);
+        }
+        $("#categories>.row").html(elems);
+    });
+}
+
+function loadSubcategories (category) {
+
+    var elems = [];
+
+    for (var i in json) {
+        if (json[i].class_id === category) {
+            for (var j in json[i].subclass) {
+                var $temp = $(".category-temp").clone();
+                $temp.find(".category-title").html(json[i].subclass[j]);
+                $temp.find(".category").attr("subcategory", json[i].subclass_id[j]);
+                $temp.find(".category").attr("category", category);
+                $temp.removeClass("category-temp");
+                $temp.show();
+                elems.push($temp);
+            }
+            break;
+        }
+    }
+    $("#categories>.row").html(elems);
+}
+
+function loadCategories () {
+
+    makeAjaxPostCall("getMappings", {}, function (data) {
+        
+        json = JSON.parse(data);
+        var elems = [];
+
+        for (var i in json) {
+            var $temp = $(".category-temp").clone();
+            $temp.find(".category-title").html(json[i].name);
+            $temp.find(".category").attr("category", json[i].class_id);
+            $temp.removeClass("category-temp");
+            $temp.show();
+            elems.push($temp);
+        }
+        $("#categories>.row").html(elems);
+    });
+}
+
+function makeAjaxPostCall (url, params, callback) {
+
+    // make the ajax request
+    $.ajax({
+        url:"http://127.0.0.1:7777/" + url,
+        type: 'post',
+        cache: false,
+        data: params,
+        success: function(data){
+            // do stuff with json (in this case an array)
+            callback(data);
+        },
+        error: function(jqXHR, textStatus, err){
+               alert('text status ' + textStatus + ', err ' + err)
+           }    
+    });
+};
+
+function showBackButton () {
+    $(".backButton").show();
+}
+
+function hideBackButton () {
+    $(".backbutton").hide();
+}
+
+function capitaliseFirstLetter (string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
